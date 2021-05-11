35f1cce52321140dee478f21f2e8a5ddbd7956e5
solenemhep <solene.pettier@gmail.com>
2021-05-11 16:16:18 +0200

    ok

--- exercices-solidity-etherum/PayableCalculator.sol
+++ exercices-solidity-etherum/PayableCalculator.sol
@@ -3,7 +3,7 @@
 pragma solidity ^0.8.0;
 
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/utils/Address.sol";
-import "./Ownable.sol";
+import "../Ownable.sol";
 
 contract CheckOdd is Ownable {
     // library usage
